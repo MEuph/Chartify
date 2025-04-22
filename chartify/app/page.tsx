93f154cc222535c60060d6463750828cc7db20fe
MEuph <christophermharris9999@gmail.com>
2025-04-21 23:59:24 -0500

    Made it so generate code button dumps flowchart data to the code output tab

--- chartify/app/page.tsx
+++ chartify/app/page.tsx
@@ -1,20 +1,30 @@
 'use client';
 
+import { useState, useRef } from 'react';
 import Header from "./components/Header";
 import LeftSidebar from "./components/LeftSidebar";
 import RightSidebar from "./components/RightSidebar";
 import IntegratedDrawIO from "./components/IntegratedDrawIO";
 
 export default function Home() {
+  const drawioRef = useRef<any>(null);
+  const [logContent, setLogContent] = useState('');
+
+  const handleGenerateCode = () => {
+    drawioRef.current?.exportXml((xml: string) => {
+      setLogContent(xml);
+    });
+  };
+
   return (
     <div className="min-h-screen bg-gray-100 text-3xl flex flex-col">
-      <Header />
+      <Header onGenerateCode={handleGenerateCode} />
       <div className="flex flex-1 w-full overflow-hidden">
         <LeftSidebar />
         <div className="flex flex-grow overflow-hidden">
-          <IntegratedDrawIO/>
+          <IntegratedDrawIO ref={drawioRef} />
         </div>
-        <RightSidebar />
+        <RightSidebar logs = {logContent} />
       </div>
     </div>
   );
