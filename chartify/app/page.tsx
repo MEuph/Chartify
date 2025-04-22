32b162cc331edeefed4c0bcfcef1fa17a7a33b98
MEuph <christophermharris9999@gmail.com>
2025-04-21 23:08:07 -0500

    Added integrated draw.io window

--- chartify/app/page.tsx
+++ chartify/app/page.tsx
@@ -3,14 +3,17 @@
 import Header from "./components/Header";
 import LeftSidebar from "./components/LeftSidebar";
 import RightSidebar from "./components/RightSidebar";
+import IntegratedDrawIO from "./components/IntegratedDrawIO";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 text-3xl flex flex-col">
       <Header />
-      <div className="flex flex-1 overflow-hidden">
+      <div className="flex flex-1 w-full overflow-hidden">
         <LeftSidebar />
-        <div className="flex-1 p-10 overflow-auto">{/* Main content goes here */}</div>
+        <div className="flex flex-grow overflow-hidden">
+          <IntegratedDrawIO/>
+        </div>
         <RightSidebar />
       </div>
     </div>
