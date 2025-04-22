30044bcdc0441fa4611e44bc1e79ca0b6d9ddf18
MEuph <christophermharris9999@gmail.com>
2025-04-21 21:59:58 -0500

    Added right sidebar for code generation, logs, and errors

--- chartify/app/page.tsx
+++ chartify/app/page.tsx
@@ -1,15 +1,17 @@
 'use client';
 
 import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
+import LeftSidebar from "./components/LeftSidebar";
+import RightSidebar from "./components/RightSidebar";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 text-3xl flex flex-col">
       <Header />
       <div className="flex flex-1 overflow-hidden">
-        <Sidebar />
+        <LeftSidebar />
         <div className="flex-1 p-10 overflow-auto">{/* Main content goes here */}</div>
+        <RightSidebar />
       </div>
     </div>
   );
